72aafc9f77ac0e806287fe2f88bd91e69eb4dff0
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-08-02 05:20:34 -0400

    Render loaded feed on load completion.

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Meharoof Najeeb on 29/07/2025.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public var locationContainer = UIView()
+    public var locationLabel = UILabel()
+    public var descriptionLabel = UILabel()
+}
