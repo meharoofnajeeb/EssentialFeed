31344bf9dff727ed300f62310335d480f4a2ff1a
Meharoof Najeeb <meharoofnajeeb@gmail.com>
2025-12-06 03:34:01 -0500

    Moved ViewModels to seperate files.

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Meharoof Najeeb on 2025-12-06.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Meharoof Najeeb on 2025-12-06.
+//  Copyright © 2025 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
